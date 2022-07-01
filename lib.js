3c67009252f88f8d83733c07ec541324da137b58
AbleValiant <54870129+AbleValiant@users.noreply.github.com>
2022-07-02 02:20:06 +0530

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,135 @@
+(function (_0x18817a, _0x34c1d3) {
+  const _0xcbb805 = _0x7763,
+    _0x5a0993 = _0x18817a();
+  while (!![]) {
+    try {
+      const _0x1dba5d =
+        (-parseInt(_0xcbb805(0xfb)) / 0x1) * (parseInt(_0xcbb805(0xff)) / 0x2) +
+        -parseInt(_0xcbb805(0x110)) / 0x3 +
+        -parseInt(_0xcbb805(0x10a)) / 0x4 +
+        (parseInt(_0xcbb805(0x111)) / 0x5) * (parseInt(_0xcbb805(0xfa)) / 0x6) +
+        (parseInt(_0xcbb805(0xfc)) / 0x7) * (-parseInt(_0xcbb805(0xfd)) / 0x8) +
+        (-parseInt(_0xcbb805(0xfe)) / 0x9) *
+          (parseInt(_0xcbb805(0x10f)) / 0xa) +
+        parseInt(_0xcbb805(0x114)) / 0xb;
+      if (_0x1dba5d === _0x34c1d3) break;
+      else _0x5a0993["push"](_0x5a0993["shift"]());
+    } catch (_0x340cc7) {
+      _0x5a0993["push"](_0x5a0993["shift"]());
+    }
+  }
+})(_0x48c8, 0x7e0b9),
+  (function (_0x4e280c, _0x4756d7) {
+    const _0x2e0fb4 = _0x7763;
+    typeof exports === _0x2e0fb4(0x107) && typeof module !== _0x2e0fb4(0x102)
+      ? (module["exports"] = _0x4756d7())
+      : typeof define === _0x2e0fb4(0x112) && define[_0x2e0fb4(0x113)]
+      ? define(_0x4756d7)
+      : ((_0x4e280c = _0x4e280c || self),
+        (_0x4e280c[_0x2e0fb4(0x104)] = _0x4756d7()));
+  })(this, function () {
+    function _0x565156(_0x631fc6, _0x21918f) {
+      const _0x21820c = _0x7763;
+      (_0x21918f = _0x21918f || document[_0x21820c(0x100)]),
+        (typeof _0x631fc6 === _0x21820c(0x107)
+          ? Array[_0x21820c(0x108)](_0x631fc6)
+          : null) &&
+          _0x631fc6[_0x21820c(0x105)]((_0x2ce480) => {
+            const _0x42dfb5 = _0x21820c;
+            let _0x1d2044 = document[_0x42dfb5(0x115)](
+              _0x2ce480[_0x42dfb5(0x109)]
+            );
+            _0xc5d965(_0x1d2044, _0x2ce480[_0x42dfb5(0x116)]);
+            if (_0x2ce480[_0x42dfb5(0x10c)] !== undefined)
+              _0x2c8b7d(_0x1d2044, _0x2ce480["val"]);
+            if (_0x2ce480[_0x42dfb5(0x10e)] !== undefined)
+              new _0x1f564a(_0x2ce480[_0x42dfb5(0x10e)], _0x1d2044);
+            _0x21918f[_0x42dfb5(0x101)](_0x1d2044);
+          });
+    }
+    function _0xc5d965(_0x330688, _0x217c25) {
+      const _0x4ae481 = _0x7763;
+      for (const _0x41c68f in _0x217c25) {
+        _0x217c25["hasOwnProperty"](_0x41c68f) &&
+          _0x330688[_0x4ae481(0x10d)](_0x41c68f, _0x217c25[_0x41c68f]);
+      }
+    }
+    function _0x2c8b7d(_0x4c9344, _0x2b071c) {
+      const _0x2ac0be = _0x7763;
+      _0x4c9344[_0x2ac0be(0x106)] = _0x2b071c;
+    }
+    function _0x3d3bfb(_0x1e0f7b) {
+      const _0x3d559e = _0x7763;
+      (_0x1e0f7b["setAttributes"] = function (_0x57e841, _0x16a78f) {
+        _0xc5d965(_0x57e841, _0x16a78f);
+      }),
+        (_0x1e0f7b[_0x3d559e(0x10b)] = function (_0x334f1a, _0x32d34d) {
+          _0x2c8b7d(_0x334f1a, _0x32d34d);
+        });
+    }
+    function _0x4e6c83(_0x2759fe) {
+      const _0xcd2296 = _0x7763;
+      _0x2759fe["prototype"][_0xcd2296(0x103)] = function (
+        _0x4e4de9,
+        _0x1a86c7
+      ) {
+        let _0x34e7f = this;
+        _0x565156(_0x4e4de9, _0x1a86c7), _0x3d3bfb(_0x34e7f);
+      };
+    }
+    _0x4e6c83(_0x1f564a);
+    function _0x1f564a(_0x35351d, _0x404c65 = null) {
+      this instanceof _0x1f564a &&
+        _0x35351d != null &&
+        this["generate"](_0x35351d, _0x404c65);
+    }
+    return _0x1f564a;
+  });
+function _0x7763(_0x28c3e6, _0x396812) {
+  const _0x48c800 = _0x48c8();
+  return (
+    (_0x7763 = function (_0x7763e7, _0x23a8a8) {
+      _0x7763e7 = _0x7763e7 - 0xfa;
+      let _0x31898e = _0x48c800[_0x7763e7];
+      return _0x31898e;
+    }),
+    _0x7763(_0x28c3e6, _0x396812)
+  );
+}
+function _0x48c8() {
+  const _0x537551 = [
+    "20BbQDpG",
+    "function",
+    "amd",
+    "38494346XXSlJj",
+    "createElement",
+    "att",
+    "1004628NkeDbG",
+    "15769wihSUo",
+    "28mhROid",
+    "1360960OJsZHn",
+    "264024oDfkxh",
+    "30bMMNcl",
+    "body",
+    "appendChild",
+    "undefined",
+    "generate",
+    "HtmlGenerator",
+    "forEach",
+    "textContent",
+    "object",
+    "isArray",
+    "tag",
+    "4017860bprogv",
+    "setValue",
+    "val",
+    "setAttribute",
+    "childs",
+    "290yEVWTP",
+    "2642199UxwIjd",
+  ];
+  _0x48c8 = function () {
+    return _0x537551;
+  };
+  return _0x48c8();
+}
