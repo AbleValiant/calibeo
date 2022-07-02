53a6958a6e8568ac7199cc95be9e3c6e12384100
AbleValiant <54870129+AbleValiant@users.noreply.github.com>
2022-07-02 19:28:17 +0530

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,168 +1,247 @@
-function _0x2474() {
-  const _0x5d93de = [
-    "childs",
-    "att",
-    "12zpGSqD",
-    "file",
-    "innerHTML",
-    "random",
-    "<label\x20for=name-backend>Name\x20on\x20backend:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=name-backend>\x0a\x20\x20\x20\x20\x20\x20<label\x20for=name-frontend>Name\x20on\x20FrontEnd:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=name-frontend>",
-    "log",
-    "querySelector",
-    "source-prev",
-    "1081385AnGkxC",
-    "dataset",
-    "1706453bDfQcR",
-    "getElementById",
-    "156429WWIXoz",
-    "36fVJrgV",
-    "code",
-    "select-type",
-    "1NIaaHa",
-    "for",
-    "replaceAll",
-    "addEventListener",
-    "div",
-    "b-add",
-    "value",
-    "click",
-    "31180UndNeo",
-    "input",
-    "3733504pVsUpC",
-    "20015468vQEyeL",
-    "label",
-    "val",
-    "#name-frontend",
-    "1503DKVWyU",
-    "push",
-    "text",
-    "onchange",
-    "708746FGssgG",
-    "<label\x20for=name-backend>Name\x20on\x20backend:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=name-backend>\x0a\x20\x20\x20\x20\x20\x20<label\x20for=name-frontend>Name\x20on\x20FrontEnd:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=name-frontend>\x0a\x20\x20\x20\x20\x20\x20<label\x20for=\x22min-check\x22>Minimum\x20Limit?\x20</label>\x0a\x20\x20\x20\x20<input\x20id=\x22min-check\x22\x20type=\x22checkbox\x22>\x0a\x20\x20\x20\x20<input\x20type=\x22number\x22id=\x22min-number\x22>\x0a\x0a\x20\x20\x20\x20<label\x20for=\x22min-check\x22>Maximum\x20Limit?\x20</label>\x0a\x20\x20\x20\x20<input\x20id=\x22min-check\x22\x20type=\x22checkbox\x22>\x0a\x20\x20\x20\x20<input\x20type=\x22number\x22id=\x22min-number\x22>",
-  ];
-  _0x2474 = function () {
-    return _0x5d93de;
-  };
-  return _0x2474();
-}
-function _0x3a61(_0x52457a, _0x35be2f) {
-  const _0x247430 = _0x2474();
-  return (
-    (_0x3a61 = function (_0x3a6145, _0x1a63f3) {
-      _0x3a6145 = _0x3a6145 - 0xe4;
-      let _0x8f6e6f = _0x247430[_0x3a6145];
-      return _0x8f6e6f;
-    }),
-    _0x3a61(_0x52457a, _0x35be2f)
-  );
-}
-const _0x5b840e = _0x3a61;
-(function (_0x13018b, _0x406b74) {
-  const _0xda827a = _0x3a61,
-    _0x3a595a = _0x13018b();
-  while (!![]) {
-    try {
-      const _0x39c0bd =
-        (-parseInt(_0xda827a(0xf2)) / 0x1) *
-          (parseInt(_0xda827a(0x105)) / 0x2) +
-        (parseInt(_0xda827a(0xee)) / 0x3) * (parseInt(_0xda827a(0xef)) / 0x4) +
-        (parseInt(_0xda827a(0xea)) / 0x5) *
-          (-parseInt(_0xda827a(0x109)) / 0x6) +
-        -parseInt(_0xda827a(0xec)) / 0x7 +
-        -parseInt(_0xda827a(0xfc)) / 0x8 +
-        (parseInt(_0xda827a(0x101)) / 0x9) *
-          (-parseInt(_0xda827a(0xfa)) / 0xa) +
-        parseInt(_0xda827a(0xfd)) / 0xb;
-      if (_0x39c0bd === _0x406b74) break;
-      else _0x3a595a["push"](_0x3a595a["shift"]());
-    } catch (_0x453592) {
-      _0x3a595a["push"](_0x3a595a["shift"]());
-    }
-  }
-})(_0x2474, 0x421b7);
-let typeSelect = document[_0x5b840e(0xed)](_0x5b840e(0xf1)),
-  addBtn = document[_0x5b840e(0xed)](_0x5b840e(0xf7)),
-  optionsContainer = document[_0x5b840e(0xe8)](
-    ".options-configurer\x20.options"
-  ),
-  previewContainer = document[_0x5b840e(0xed)](_0x5b840e(0xe9));
-handleTypeChange = (_0x2fa96b) => {
-  const _0x4a768e = _0x5b840e;
-  optionsContainer[_0x4a768e(0xe4)] = "";
-  switch (_0x2fa96b) {
+let typeSelect = document.getElementById("select-type");
+let addBtn = document.getElementById("b-add");
+let exportBtn = document.getElementById("b-export");
+let optionsContainer = document.querySelector(".options-configurer .options");
+let previewContainer = document.getElementById("source-prev");
+
+handleTypeChange = (inputCode) => {
+  optionsContainer.innerHTML = "";
+  switch (inputCode) {
     case "0":
-      (optionsContainer["dataset"][_0x4a768e(0xf0)] = "0"),
-        (optionsContainer[_0x4a768e(0xe4)] =
-          "<label\x20for=name-backend>Name\x20on\x20backend:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=name-backend>\x0a\x20\x20\x20\x20\x20\x20<label\x20for=name-frontend>Name\x20on\x20FrontEnd:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=name-frontend>\x0a\x20\x20\x20\x20\x20\x20<label\x20for=\x22min-check\x22>Minimum\x20Limit?\x20</label>\x0a\x20\x20\x20\x20<input\x20id=\x22min-check\x22\x20type=\x22checkbox\x22>\x0a\x20\x20\x20\x20<input\x20type=\x22number\x22id=\x22min-number\x22>\x0a\x0a\x20\x20\x20\x20<label\x20for=\x22min-check\x22>Maximum\x20Limit?\x20</label>\x0a\x20\x20\x20\x20<input\x20id=\x22min-check\x22\x20type=\x22checkbox\x22>\x0a\x20\x20\x20\x20<input\x20type=\x22number\x22id=\x22min-number\x22>");
+      optionsContainer.dataset.code = "0";
+      optionsContainer.innerHTML = `<label for=name-backend>Name on backend: </label>
+      <input id=name-backend>
+      <label for=name-frontend>Name on FrontEnd: </label>
+      <input id=name-frontend>
+      <label for="min-check">Minimum Limit? </label>
+    <input id="min-check" type="checkbox">
+    <input type="number"id="min-number">
+
+    <label for="max-check">Maximum Limit? </label>
+    <input id="max-check" type="checkbox">
+    <input type="number"id="max-number">`;
       break;
     case "1":
-      (optionsContainer[_0x4a768e(0xeb)][_0x4a768e(0xf0)] = "1"),
-        (optionsContainer[_0x4a768e(0xe4)] = _0x4a768e(0x106));
+      optionsContainer.dataset.code = "1";
+      optionsContainer.innerHTML = `<label for=name-backend>Name on backend: </label>
+      <input id=name-backend>
+      <label for=name-frontend>Name on FrontEnd: </label>
+      <input id=name-frontend>
+      <label for="min-check">Minimum Limit? </label>
+    <input id="min-check" type="checkbox">
+    <input type="number"id="min-number">
+
+    <label for="max-check">Maximum Limit? </label>
+    <input id="max-check" type="checkbox">
+    <input type="number"id="max-number">`;
       break;
     case "2":
-      (optionsContainer[_0x4a768e(0xeb)][_0x4a768e(0xf0)] = "2"),
-        (optionsContainer[_0x4a768e(0xe4)] = _0x4a768e(0xe6));
+      optionsContainer.dataset.code = "2";
+      optionsContainer.innerHTML = `<label for=name-backend>Name on backend: </label>
+      <input id=name-backend>
+      <label for=name-frontend>Name on FrontEnd: </label>
+      <input id=name-frontend>`;
       break;
     case "3":
-      (optionsContainer[_0x4a768e(0xeb)][_0x4a768e(0xf0)] = "3"),
-        (optionsContainer["innerHTML"] = _0x4a768e(0x106));
+      optionsContainer.dataset.code = "3";
+      optionsContainer.innerHTML = `<label for=name-backend>Name on backend: </label>
+      <input id=name-backend>
+      <label for=name-frontend>Name on FrontEnd: </label>
+      <input id=name-frontend>
+      <label for="min-check">Minimum Limit? </label>
+    <input id="min-check" type="checkbox">
+    <input type="number"id="min-number">
+
+    <label for="max-check">Maximum Limit? </label>
+    <input id="max-check" type="checkbox">
+    <input type="number"id="max-number">`;
       break;
     case "4":
-      (optionsContainer[_0x4a768e(0xeb)][_0x4a768e(0xf0)] = "4"),
-        (optionsContainer[_0x4a768e(0xe4)] =
-          "<label\x20for=\x22name-backend\x22>Name\x20on\x20backend:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=\x22name-backend\x22\x20type=\x22text\x22>\x0a\x20\x20\x0a\x20\x20\x20\x20\x20\x20<label\x20for=\x22name-frontend\x22>Name\x20on\x20FrontEnd:\x20</label>\x0a\x20\x20\x20\x20\x20\x20<input\x20id=\x22name-frontend\x22\x20type=\x22text\x22>");
+      optionsContainer.dataset.code = "4";
+      optionsContainer.innerHTML = `<label for="name-backend">Name on backend: </label>
+      <input id="name-backend" type="text">
+  
+      <label for="name-frontend">Name on FrontEnd: </label>
+      <input id="name-frontend" type="text">`;
       break;
     default:
-      (optionsContainer["dataset"]["code"] = ""),
-        (optionsContainer["innerHTML"] =
-          "<h1>Error:\x20Doesn\x27t\x20seem\x20valid!</h1>");
+      optionsContainer.dataset.code = "";
+      optionsContainer.innerHTML = "<h1>Error: Doesn't seem valid!</h1>";
   }
 };
+
 let elementsArray = [];
-(renderPreview = () => {
-  const _0x3efe66 = _0x5b840e;
-  (previewContainer["innerHTML"] = ""),
-    console[_0x3efe66(0xe7)](elementsArray),
-    new HtmlGenerator(elementsArray, previewContainer);
-}),
-  (handleAddition = () => {
-    const _0x3c1513 = _0x5b840e;
-    let _0x5e3801 = optionsContainer[_0x3c1513(0xeb)][_0x3c1513(0xf0)],
-      _0x269c3e = {
-        tag: _0x3c1513(0xf6),
-        att: { class: "custom_prop" },
-        childs: [{ tag: _0x3c1513(0xfe), att: {} }],
+
+handleExport = () => {
+  /* Get the text field */
+  var copyText = document.getElementById("code-export");
+  copyText.value = previewContainer.innerHTML;
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  alert("Copied the exported code!");
+};
+
+renderPreview = () => {
+  previewContainer.innerHTML = "";
+  console.log(elementsArray);
+  new HtmlGenerator(elementsArray, previewContainer);
+  applyExtraListeneres();
+};
+
+handleAddition = () => {
+  let configurationCode = optionsContainer.dataset.code;
+
+  let element = {
+    tag: "div",
+    att: { class: "custom_prop" },
+    childs: [
+      {
+        tag: "label",
+        att: {},
       },
-      _0x1443c5 =
-        optionsContainer[_0x3c1513(0xe8)]("#name-frontend")[_0x3c1513(0xf8)][
-          _0x3c1513(0xf4)
-        ]("\x20", "") + Math["round"](Math[_0x3c1513(0xe5)]() * 0x3e8);
-    (_0x269c3e["childs"][0x0][_0x3c1513(0x108)][_0x3c1513(0xf3)] = _0x1443c5),
-      (_0x269c3e[_0x3c1513(0x107)][0x0][_0x3c1513(0xff)] = optionsContainer[
-        _0x3c1513(0xe8)
-      ](_0x3c1513(0x100))[_0x3c1513(0xf8)]);
-    if (_0x5e3801 == 0x0 || _0x5e3801 == 0x1 || _0x5e3801 == 0x3) {
-      let _0x1a173a;
-      if (_0x5e3801 == 0x0) _0x1a173a = _0x3c1513(0x103);
-      else
-        _0x5e3801 == 0x1
-          ? (_0x1a173a = "number")
-          : (_0x1a173a = _0x3c1513(0x103));
-      let _0x6d9839 = { tag: _0x3c1513(0xfb), att: { type: _0x1a173a } };
-      _0x269c3e[_0x3c1513(0x107)][_0x3c1513(0x102)](_0x6d9839);
+    ],
+  };
+
+  let inputId =
+    optionsContainer.querySelector("#name-frontend").value.replaceAll(" ", "") +
+    Math.round(Math.random() * 1000);
+
+  element.childs[0].att.for = inputId;
+  element.childs[0].val =
+    optionsContainer.querySelector("#name-frontend").value;
+
+  if (
+    configurationCode == 0 ||
+    configurationCode == 1 ||
+    configurationCode == 3
+  ) {
+    let inputElement = {
+      tag: "input",
+      att: {},
+    };
+
+    inputElement.att.name = `properties[${
+      optionsContainer.querySelector("#name-backend").value
+    }]`;
+
+    let actualTag;
+    if (configurationCode == 0) {
+      actualTag = "text";
+      inputElement.att.class = "cust_prop letters-only";
+
+      // min max
+      if (optionsContainer.querySelector("#min-check").checked) {
+        inputElement.att.pattern = `.{${
+          optionsContainer.querySelector("#min-number").value
+        },}`;
+        // inputElement.att.min =
+        //   optionsContainer.querySelector("#min-number").value;
+      }
+      if (optionsContainer.querySelector("#max-check").checked) {
+        inputElement.att.pattern = `.{0,${
+          optionsContainer.querySelector("#max-number").value
+        }}`;
+        // inputElement.att.max =
+        //   optionsContainer.querySelector("#max-number").value;
+      }
+      if (
+        optionsContainer.querySelector("#max-check").checked &&
+        optionsContainer.querySelector("#min-check").checked
+      ) {
+        inputElement.att.pattern = `.{${
+          optionsContainer.querySelector("#min-number").value
+        },${optionsContainer.querySelector("#max-number").value}}`;
+        // inputElement.att.max =
+        //   optionsContainer.querySelector("#max-number").value;
+      }
+    } else if (configurationCode == 1) {
+      actualTag = "number";
+      inputElement.att.class = "cust_prop";
+      if (optionsContainer.querySelector("#min-check").checked) {
+        inputElement.att.min =
+          optionsContainer.querySelector("#min-number").value;
+      }
+      if (optionsContainer.querySelector("#max-check").checked) {
+        inputElement.att.max =
+          optionsContainer.querySelector("#max-number").value;
+      }
+    } else {
+      actualTag = "text";
+      inputElement.att.class = "cust_prop";
+      // min max
+      if (optionsContainer.querySelector("#min-check").checked) {
+        inputElement.att.min =
+          optionsContainer.querySelector("#min-number").value;
+      }
+      if (optionsContainer.querySelector("#max-check").checked) {
+        inputElement.att.max =
+          optionsContainer.querySelector("#max-number").value;
+      }
     }
-    if (_0x5e3801 == 0x2 || _0x5e3801 == 0x4) {
-      let _0x4cec1e;
-      if (_0x5e3801 == 0x2) _0x4cec1e = "date";
-      else _0x5e3801 == 0x4 && (_0x4cec1e = _0x3c1513(0x10a));
-      let _0x18d1e6 = { tag: _0x3c1513(0xfb), att: { type: _0x4cec1e } };
-      _0x269c3e[_0x3c1513(0x107)][_0x3c1513(0x102)](_0x18d1e6);
+
+    inputElement.att.type = actualTag;
+
+    // set max min
+
+    element.childs.push(inputElement);
+  }
+
+  if (configurationCode == 2 || configurationCode == 4) {
+    let inputElement = {
+      tag: "input",
+      att: {},
+    };
+    let actualTag;
+    if (configurationCode == 2) {
+      actualTag = "date";
+      inputElement.att.class = "cust_prop";
+    } else if (configurationCode == 4) {
+      actualTag = "file";
+      inputElement.att.class = "cust_prop";
     }
-    elementsArray["push"](_0x269c3e), renderPreview();
-  }),
-  (typeSelect[_0x5b840e(0x104)] = function () {
-    const _0x2625aa = _0x5b840e;
-    handleTypeChange(this[_0x2625aa(0xf8)]);
-  }),
-  handleTypeChange("0"),
-  addBtn[_0x5b840e(0xf5)](_0x5b840e(0xf9), handleAddition);
+
+    inputElement.att.type = actualTag;
+
+    inputElement.att.name = `properties[${
+      optionsContainer.querySelector("#name-backend").value
+    }]`;
+
+    element.childs.push(inputElement);
+  }
+  elementsArray.push(element);
+  renderPreview();
+};
+
+typeSelect.onchange = function () {
+  handleTypeChange(this.value);
+};
+
+handleTypeChange("0");
+
+addBtn.addEventListener("click", handleAddition);
+exportBtn.addEventListener("click", handleExport);
+
+// Functionality for various types of input
+
+function applyExtraListeneres() {
+  // letters only
+  document
+    .querySelectorAll("input.cust_prop.letters-only")
+    .forEach(function (input) {
+      input.oninput = function () {
+        let reg = /^[a-z]+$/i;
+        if (reg.test(this.value.charAt(this.value.length - 1))) {
+          this.value = this.value;
+        } else {
+          this.value = this.value.slice(0, -1);
+        }
+      };
+    });
+}
